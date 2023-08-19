0324193a994ab6180d31072752ef36e22b4be713
erica-luong <93899262+erica-luong@users.noreply.github.com>
2023-08-18 19:07:02 -0700

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -38,7 +38,7 @@ def main():
             st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
 
         # Display the podcast guest and their details in a side-by-side layout
-        col3 = st.columns(7)
+        col3 = st.columns([7])
 
         with col3:
             st.subheader("Podcast Guest")
@@ -83,7 +83,7 @@ def main():
             st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
 
         # Display the podcast guest and their details in a side-by-side layout
-        col3 = st.columns(7)
+        col3 = st.columns([7])
 
         with col3:
             st.subheader("Podcast Guest")
